01bac17ad09bc759150ceb6231d9a393b200a0cf
Prachi524 <86963462+Prachi524@users.noreply.github.com>
2026-02-05 22:52:53 +0530

    Test

---
+++ q4.py
@@ -0,0 +1,28 @@
+interior_area = float(input("Enter interior wall area:"))
+interior_rate = float(input("Enter cost:"))
+exterior_area = float(input("Enter exterior wall area:"))
+exterior_rate = float(input("Enter cost:"))
+
+interior_cost = interior_area * interior_rate
+
+exterior_cost = exterior_area * exterior_rate
+
+total_cost = interior_cost + exterior_cost
+
+print("Interior painting cost:",interior_cost)
+print("Exterior cost:",exterior_cost)
+print("total:",total_cost)
+
+
+
+
+
+                      
+
+                      
+
+                      
+
+                      
+
+                      
