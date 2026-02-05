7844c771f186ff5cb2234feda9090ffdfcbc2b9c
Prachi524 <86963462+Prachi524@users.noreply.github.com>
2026-02-05 23:01:31 +0530

    Test 1 Questions

---
+++ core_python/test/test_1/q2.py
@@ -0,0 +1,10 @@
+
+
+p = float(input("Enter principal amount:"))
+t = float(input("Enter the time:"))
+r = float(input("Enter rate of interest:"))
+
+si = (p * t * r) / 100
+
+print(f"Simple Interest = {si}")
+
---
+++ core_python/test/test_1/q3.py
@@ -0,0 +1,8 @@
+km = float(input("Enter the kilometer"))
+
+meter = km * 1000
+
+centimeter = km * 10000
+
+print("Distance in meter:",meter)
+print("distance in centimeter:",centimeter)
